1f7e1f8b58973cc4926aad2ef1ce2a80560247a2
mozta <perezaguirrerafael@gmail.com>
2024-06-05 20:29:32 -0600

    fix deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: 'https://mozta.github.io/frontend-pinata-control/'
 })
